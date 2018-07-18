da5d5e5679239093844665d1e991d94a88e18756
Shahjada Talukdar <shahjada.talukdar@gmail.com>
2018-07-18 15:56:57 +0200

    Test added

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -1,21 +1,16 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from '../src/App';
-
-// it('renders without crashing', () => {
-//   const div = document.createElement('div');
-//   ReactDOM.render(<App />, div);
-//   ReactDOM.unmountComponentAtNode(div);
-// });
-
-
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { shallow } from 'enzyme';
 import App from '../src/App';
 
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
 it('renders welcome message', () => {
   const wrapper = shallow(<App />);
-  const welcome = <h1>Welcome to React</h1>;
-  // expect(wrapper.contains(welcome)).to.equal(true);
+  const welcome = <h1 className="App-title">Welcome to React</h1>;
   expect(wrapper.contains(welcome)).toEqual(true);
 });
